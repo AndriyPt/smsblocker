7139aae935cb4b0b661790237951e266a391dc25
AndriyPt@gmail.com <AndriyPt@gmail.com@fab42c06-c640-2415-3db7-d70ee238fa37>
2013-06-23 21:39:14 +0000

    Removed menu from resources and code.

--- src/org/sms/blocker/ApplicationSettings.java
+++ src/org/sms/blocker/ApplicationSettings.java
@@ -2,7 +2,6 @@ package org.sms.blocker;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
 
 public class ApplicationSettings extends Activity {
 
@@ -11,13 +10,4 @@ public class ApplicationSettings extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_application_settings);
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.application_settings, menu);
-        return true;
-    }
-    
 }
