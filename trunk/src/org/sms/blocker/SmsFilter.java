9bd820567f439a93f127a934c81b50ef76986357
AndriyPt@gmail.com <AndriyPt@gmail.com@fab42c06-c640-2415-3db7-d70ee238fa37>
2013-06-27 15:20:00 +0000

    Added code to write deleted SMS to log file and to read this log.

--- trunk/src/org/sms/blocker/SmsFilter.java
+++ trunk/src/org/sms/blocker/SmsFilter.java
@@ -1,5 +1,16 @@
 package org.sms.blocker;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.sms.blocker.constant.GeneralConstants;
 import org.sms.blocker.settings.UserSettings;
 
 import android.content.BroadcastReceiver;
@@ -25,10 +36,61 @@ public class SmsFilter extends BroadcastReceiver {
             SmsMessage message = SmsMessage.createFromPdu((byte[])pdus[0]);
 
             if (userSettings.getBlacklist().contains(message.getDisplayOriginatingAddress())) {
+
                 Log.i(TAG, "Deleting SMS from \"" + message.getDisplayOriginatingAddress()
                     + "\" with following text \"" + message.getMessageBody() + "\"");
+
+                String smsText = message.getMessageBody();
+                smsText = smsText.replace("\r", "").replace("\n", "");
+                final String messageInfo = message.getDisplayOriginatingAddress() + " => " + smsText;
+
+                writeMessageToLogFile(context, messageInfo);
+
                 abortBroadcast();
             }
+        }
+    }
+
+    private void writeMessageToLogFile(final Context context, final String messageInfo) {
+
+        final File file = new File(context.getFilesDir(), GeneralConstants.DELETED_SMS_LOG_FILE_NAME);
+
+        final List<String> fileContent = new ArrayList<String>();
+        if (file.exists()) {
+            BufferedReader inputStream = null;
+            try {
+                inputStream = new BufferedReader(new FileReader(file));
+                String line = null;
+                while (null != (line = inputStream.readLine())) {
+                    fileContent.add(line);
+                }
+                inputStream.close();
+            }
+            catch (FileNotFoundException e) {
+                Log.e(TAG, e.getMessage());
+            }
+            catch (IOException e) {
+                Log.e(TAG, e.getMessage());
+            }
+        }
+        fileContent.add(messageInfo);
+
+        while (fileContent.size() > GeneralConstants.LOG_FILE_MAX_LINES_COUNT) {
+            fileContent.remove(0);
+        }
+
+        final BufferedWriter outputStream;
+
+        try {
+            outputStream = new BufferedWriter(new FileWriter(file));
+            for (int i = 0; i < fileContent.size(); i++) {
+                outputStream.write(fileContent.get(i));
+                outputStream.newLine();
+            }
+            outputStream.close();
+        }
+        catch (Exception e) {
+            Log.e(TAG, e.getMessage());
         }
     }
 }
