0b1bf5bcd6648a1fd6689f360640849d1bf2c93c
AndriyPt@gmail.com <AndriyPt@gmail.com@fab42c06-c640-2415-3db7-d70ee238fa37>
2013-07-02 20:36:25 +0000

    Added code to start service in case if application was updated.

--- trunk/src/org/sms/blocker/receiver/BootCompleteBroadcastReceiver.java
+++ trunk/src/org/sms/blocker/receiver/BootCompleteBroadcastReceiver.java
@@ -6,16 +6,36 @@ import org.sms.blocker.settings.UserSettings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class BootCompleteBroadcastReceiver extends BroadcastReceiver {
+    
+    private static final String TAG = BootCompleteBroadcastReceiver.class.getSimpleName();
+    
+    private void startServiceIfNeeded(final Context context) {
+        final UserSettings userSettings = new UserSettings(context);
+        if (userSettings.isTurnedOn()) {
+            Log.d(TAG, "Starting service...");
+            final Intent startServiceIntent = new Intent(context, BroadcastListeningService.class);
+            context.startService(startServiceIntent);
+        }
+    }
 
     @Override
     public void onReceive(final Context context, final Intent intent) {
 
-        final UserSettings userSettings = new UserSettings(context);
-        if (userSettings.isTurnedOn()) {
-            final Intent startServiceIntent = new Intent(context, BroadcastListeningService.class);
-            context.startService(startServiceIntent);
+        if ((null != intent) && (null != intent.getAction())) {
+
+            if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+                Log.d(TAG, "Processing BOOT_COMPLETED event...");
+                startServiceIfNeeded(context);
+            }
+            else if (intent.getAction().equals("android.intent.action.PACKAGE_RESTARTED")) {
+                if ((null != intent.getDataString()) && (intent.getDataString().contains("org.sms.blocker"))) {
+                    Log.d(TAG, "Processing PACKAGE_RESTARTED event for current package...");
+                    startServiceIfNeeded(context);
+                }
+            }
         }
     }
 
